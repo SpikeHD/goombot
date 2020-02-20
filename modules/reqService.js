986a9a431e5ba2574ddb029be14937f0c8fa38f8
SpikeGD <spikegdofficial@gmail.com>
2020-02-19 16:50:15 -0800

    Http request handler (#3)
    * Implement logger
    
    * Implement eslint
    
    * Add basis for HTTP handler
    
    * Use querystring to parse into object
    
    * Use arrow functions
    
    Co-authored-by: Tanner <tannersmithasdf@hotmail.com>

---
+++ modules/reqService.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const { parse } = require('querystring')
+
+exports.startService = (logger) => {
+  http.createServer((req, res) => {
+    if (req.method === 'POST') {
+      logger.log('POST: ' + JSON.stringify(parse(req.url)), 'debug')
+    }
+
+    res.end()
+  }).listen(3000)
+
+  logger.log('Backend connection service ready', 'ready')
+}
