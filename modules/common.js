778ff7869bad3e07c80fd9f28ba5b51eb1ea1697
SpikeGD <spikegdofficial@gmail.com>
2020-02-19 17:59:37 -0800

    Export common functions (#4)
    * Export loadPrefixes function to common.js
    
    * Add common.js

---
+++ modules/common.js
@@ -0,0 +1,12 @@
+exports.loadPrefixes = (client) => {
+  client.mysql.getConnection(function (err, conn) {
+    if (err) throw err
+    client.logger.log('MySQL Database Ready', 'ready')
+
+    // Gets all prefixes
+    conn.query(`SELECT GuildID,Prefix FROM ${client.config.dbname}.guilds`, (err, result) => {
+      if (err) throw err
+      client.guildPrefixes = Object.values(JSON.parse(JSON.stringify(result)))
+    })
+  })
+}
