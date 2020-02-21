2e374496a7239a49c787c332f90b2f6931fb7add
spikegd <spikegdofficial@gmail.com>
2020-02-21 13:46:12 -0800

    Add urban dictionary command

---
+++ commands/ud.js
@@ -0,0 +1,30 @@
+const https = require('https')
+const { RichEmbed } = require('discord.js')
+
+exports.run = (client, message, args) => {
+  args.shift()
+
+  var q = args.join('%20')
+
+  https.get('https://api.urbandictionary.com/v0/define?page=1&term=' + q, (res) => {
+    var data = ''
+
+    res.on('data', (chunk) => {
+      data += chunk
+    })
+
+    res.on('end', () => {
+      var obj = JSON.parse(data).list[0]
+
+      var embed = new RichEmbed()
+        .setColor('BLUE')
+        .setTitle(obj.word + ' - Top Definition with ' + obj.thumbs_up + ' points')
+        .setDescription(obj.definition + '\n\n[Permalink](' + obj.permalink + ')')
+        .addField('Example', obj.example)
+        .setFooter('Written on: ' + obj.written_on)
+
+      message.channel.send(embed)
+      console.log(obj)
+    })
+  })
+}
