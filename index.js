9dae187096da3c2ad33609adcd23e5daa1330b17
SpikeGD <spikegdofficial@gmail.com>
2020-02-22 14:20:47 -0800

    Outage logging (#7)
    * Add outage logging and entry creation
    
    * Change seconds to minutes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const handler = require('./modules/handler.js')
 const common = require('./modules/common.js')
+const moment = require('moment')
 const Discord = require('discord.js')
 const client = new Discord.Client()
 
@@ -23,10 +24,19 @@ handler.load(client)
 
 client.once('ready', () => {
   client.logger.log('Client Ready', 'ready')
-
   client.server.startService(client)
-
   common.loadPrefixes(client)
+
+  // Check and update outages
+  client.mysql.query(`UPDATE bot_crashes SET onlineTime=${Date.now()} WHERE onlineTime=0`, (err, row) => {
+    if (err) throw err
+    if (row.affectedRows !== 0) {
+      client.mysql.query('SELECT * FROM bot_crashes ORDER BY onlineTime DESC LIMIT 1', (err, rows) => {
+        if (err) throw err
+        client.logger.log('Detected outage that lasted ' + moment.duration(rows[0].onlineTime - rows[0].crashTime).asMinutes() + ' seconds', 'warn')
+      })
+    }
+  })
 })
 
 client.on('message', (message) => {
@@ -39,3 +49,16 @@ client.on('message', (message) => {
 })
 
 client.login(client.config.token)
+
+process.on('uncaughtException', function (err) {
+  client.logger.log('Caught exception: ' + err, 'error')
+
+  // Assume MySQL is still working
+  client.mysql.query(`INSERT INTO bot_crashes(crashTime) VALUES(${Date.now()})`, (err, row) => {
+    if (err) process.exit()
+    if (row) {
+      client.logger.log('Outage entry created. See you in the afterlife o7', 'log')
+      process.exit()
+    }
+  })
+})
